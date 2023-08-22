e4f6a06fe4f1ef1dbe364440c59e1ec80e5018b2
NILESHSINGH234 <nilesh01june@gmail.com>
2023-08-22 16:52:32 +0530

    feat:write a feature for local storage and delete notes

--- index.js
+++ index.js
@@ -19,3 +19,27 @@ createBtn.addEventListener("click",()=>{
     img.src="images/delete.png";
     notesContainer.appendChild(inputBox).appendChild(img);
 })
+notesContainer.addEventListener("click",function(e)
+{
+    if(e.target.tagName==="IMG")
+    {
+        e.target.parentElement.remove();
+        updateStorage();
+    }
+    else if(e.target.tagName==="p")
+    {
+        notes=document.querySelectorAll(".input-box");
+        notes.forEach(nt=>{
+            nt.onkeyup=function()
+            {
+                updateStorage();
+            }
+        })
+    }
+})
+document.addEventListener("keydown",event=>{
+    if(event.key==="Enter"){
+        document.execCommand("insertLineBreak");
+        event.preventDefault();
+    }
+})
